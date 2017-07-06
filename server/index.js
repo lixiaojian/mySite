e1b04be77582278b4de63ed137ec09e3c0ea7ef9
xiaojianli <376127890@qq.com>
2017-07-06 16:29:41 +0800

    服务端渲染

--- server/index.js
+++ server/index.js
@@ -8,6 +8,15 @@ var MainPage = require('../src/index/scripts/mainPage');
 
 var ReactDOM = require('react-dom');
 
+const timeStemp = Date.now();
+
+const loaddingScript = `document.onreadystatechange = function (){
+    if(document.readyState === "complete"){
+        document.getElementById('page_loadding').classList.add('loadding-hide');
+    }
+}`;
+
+const loaddingStyle='.page-loadding{position: fixed;top: 0;bottom: 0;left: 0;right: 0;background-color: #fff;z-index: 100;}'
 
 module.exports = function(props) {
 
@@ -19,14 +28,35 @@ module.exports = function(props) {
         <html>
         <head>
             <meta charSet="UTF-8"></meta>
+            <meta httpEquiv="X-UA-Compatible" content="edge"/>
             <meta name="baidu-site-verification" content="1gvq2Llcn4" />
+            <meta content="李晓健个人主页，李晓健WEB前端开发工程师简历，前端开发" name="description" />
+            <meta content="web前端，F2E，web前端个人简历，前端开发，李晓健,工程师,WEB前端,H5开发,简历,工作经验,WEB前端开发工程师" name="keywords" />
+            <title>李晓健个人主页</title>
+            <link href="/favicon.ico" rel="icon" type="image/x-icon" />
+            <link rel="stylesheet" href={"/build/css/index.css?v=" + timeStemp} />
+            <script dangerouslySetInnerHTML={
+                {__html: loaddingScript}
+            }></script>
+            <style dangerouslySetInnerHTML={
+                {__html:loaddingStyle}
+            }></style>
         </head>
         <body>
+        <div id="page_loadding" className="page-loadding">
+            <div className="cssload-thecube">
+                <div className="cssload-cube cssload-c1"></div>
+                <div className="cssload-cube cssload-c2"></div>
+                <div className="cssload-cube cssload-c4"></div>
+                <div className="cssload-cube cssload-c3"></div>
+            </div>
+        </div>
+
         <div id="app" dangerouslySetInnerHTML={
             {__html: content}
         } />
-        <script src="/build/js/vendor.js"></script>
-        <script src="/build/js/index.js"></script>
+        <script src={"/build/js/vendor.js?v="+ timeStemp}></script>
+        <script src={"/build/js/index.js?v=" + timeStemp}></script>
         </body>
         </html>
     );
